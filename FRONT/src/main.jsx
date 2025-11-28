12cb9ba781f8f1d461a5c152ddb36d849253453c
margar3tas <k.mikae.lieb@gmail.com>
2025-11-28 23:49:43 +0000

    working on back

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { io } from 'socket.io-client';
+
+// SOCKET TEST FOR BACKEND
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+  console.log('connected:', socket.id);
+});
+
+socket.emit('ping');
+socket.on('pong', () => console.log('PONG'));
+//
+//
+//
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
