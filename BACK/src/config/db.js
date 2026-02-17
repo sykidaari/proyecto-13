589ba469202b547a672f0191b2585d1275f3b61c
margar3tas <k.mikae.lieb@gmail.com>
2026-02-17 17:21:47 +0000

    fixed problem connecting to db, manually set dns server

--- BACK/src/config/db.js
+++ BACK/src/config/db.js
@@ -1,4 +1,9 @@
 import mongoose from 'mongoose';
+import dns from 'node:dns/promises';
+
+// ! important, fixes a problem with node DNS server, can't connect in all environments without this line. info in link:
+//https://www.mongodb.com/community/forums/t/error-querysrv-econnrefused-mongodb/259042/3
+dns.setServers(['1.1.1.1', '1.0.0.1']);
 
 const connectDB = async () => {
   try {
@@ -6,7 +11,7 @@ const connectDB = async () => {
     console.log('connected succesfully to DB');
   } catch (error) {
     console.log(process.env.DB_URL);
-    console.log(`error connecting to DB: ${error.message}`);
+    console.log(`error connecting to DB: ${error}`);
   }
 };
 
